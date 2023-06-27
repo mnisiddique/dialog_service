eccf22bbfce51347df72e1b54d628d2d6e8303fe
Prince <mnisiddique30@gmail.com>
2023-06-27 20:22:00 +0600

    feat: adds another implementation of alert service desc: This implementation is able to detect android system back button tap event in multiple navigator environment.

--- lib/src/alert_service.dart
+++ lib/src/alert_service.dart
@@ -1,8 +1,10 @@
+import 'package:dialog_service/src/system_back_button_aware_wdget.dart';
 import 'package:flutter/material.dart';
 
 import 'app_dialog.dart';
 
 abstract class AlertService {
+  bool get isAlertShowing;
   Future<T?> showAlert<T>(
     BuildContext context,
     AppAlert alert,
@@ -10,16 +12,16 @@ abstract class AlertService {
 }
 
 class AlertServiceImpl implements AlertService {
-  bool isShowing = false;
+  bool _isShowing = false;
   @override
   Future<T?> showAlert<T>(
     BuildContext context,
     AppAlert alert,
   ) {
-    if (isShowing) {
+    if (_isShowing) {
       return Future.value();
     }
-    isShowing = true;
+    _isShowing = true;
     return showDialog<T>(
       context: context,
       barrierDismissible: alert.config.barrierDismissible,
@@ -31,8 +33,43 @@ class AlertServiceImpl implements AlertService {
       anchorPoint: alert.config.anchorPoint,
       builder: (BuildContext context) => alert.build(),
     ).then((value) {
-      isShowing = false;
+      _isShowing = false;
       return value;
     });
   }
+
+  @override
+  bool get isAlertShowing => _isShowing;
+}
+
+class AlertServiceSystemBackInterceptionAwareImpl implements AlertService {
+  bool _isShowing = false;
+  @override
+  Future<T?> showAlert<T>(
+    BuildContext context,
+    AppAlert alert,
+  ) {
+    if (_isShowing) {
+      return Future.value();
+    }
+    _isShowing = true;
+    return showDialog<T>(
+      context: context,
+      barrierDismissible: alert.config.barrierDismissible,
+      barrierColor: alert.config.barrierColor,
+      barrierLabel: alert.config.barrierLabel,
+      useSafeArea: alert.config.useSafeArea,
+      useRootNavigator: alert.config.useRootNavigator,
+      routeSettings: alert.config.routeSettings,
+      anchorPoint: alert.config.anchorPoint,
+      builder: (BuildContext context) =>
+          SystemBackInterceptionAwareWidget(child: alert.build()),
+    ).then((value) {
+      _isShowing = false;
+      return value;
+    });
+  }
+
+  @override
+  bool get isAlertShowing => _isShowing;
 }
